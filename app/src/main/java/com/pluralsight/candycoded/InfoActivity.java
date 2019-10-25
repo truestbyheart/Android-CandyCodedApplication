8c3752755dc629d2a30a2b684fd06798dd5fd0f7
Daniel mwangila <daniela.mwangila@Danielas-MacBook-Pro.local>
2019-10-25 10:52:48 +0200

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,9 +1,12 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -13,6 +16,7 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        TextView adress = (TextView) findViewById(R.id.text_view_address);
 
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
         ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
@@ -20,12 +24,25 @@ public class InfoActivity extends AppCompatActivity {
                 load(uri).
                 into(candyStoreImageView);
 
-
+        adress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createMapIntent(view);
+            }
+        });
     }
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+
+    public void createMapIntent(View view){
+        Uri gmAdress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmAdress);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if(mapIntent.resolveActivity(getPackageManager()) != null) { startActivity(mapIntent); }
+
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
